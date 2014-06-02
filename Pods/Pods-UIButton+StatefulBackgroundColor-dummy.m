602dc172ccd75c246f9eda3a616d989ce48586cc
Jai Govindani <jai@zodio.com>
2014-06-02 15:03:00 +0700

    Update Cocoapods-based assets and files
    Signed-off-by: Jai Govindani <jai@zodio.com>

---
+++ Pods/Pods-UIButton+StatefulBackgroundColor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIButton_StatefulBackgroundColor : NSObject
+@end
+@implementation PodsDummy_Pods_UIButton_StatefulBackgroundColor
+@end
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// UIButton+StatefulBackgroundColor
+#define COCOAPODS_POD_AVAILABLE_UIButton_StatefulBackgroundColor
+#define COCOAPODS_VERSION_MAJOR_UIButton_StatefulBackgroundColor 0
+#define COCOAPODS_VERSION_MINOR_UIButton_StatefulBackgroundColor 1
+#define COCOAPODS_VERSION_PATCH_UIButton_StatefulBackgroundColor 3
+
